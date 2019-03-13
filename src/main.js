543a989ebb41495fea3267eac33b30c343ae34df
MateusSilveira98 <mateus.costa98.dev@gmail.com>
2019-03-13 19:36:03 -0300

    Create enviroment

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from '@/App.vue'
 import router from '@/routes/index'
 import store from '@/store/index'
 import '@/registerServiceWorker'
+import axios from 'axios'
+import enviroment from './enviroment'
 
+axios.defaults.baseURL = enviroment.getEnv();
 Vue.config.productionTip = false
 
 new Vue({
